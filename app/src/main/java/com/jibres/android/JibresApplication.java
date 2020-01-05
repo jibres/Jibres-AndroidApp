b04e0773bc25d1ac140338a051bbf77157f84f03
amingoli78 <amin.goli78@gmail.com>
2020-01-05 19:39:19 +0330

    add JibresApplication

---
+++ app/src/main/java/com/jibres/android/JibresApplication.java
@@ -0,0 +1,50 @@
+package com.jibres.android;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class JibresApplication extends Application {
+
+    public static final String TAG = JibresApplication.class.getSimpleName();
+    private RequestQueue mRequestQueue;
+    private static JibresApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized JibresApplication getInstance() {
+        return mInstance;
+    }
+
+    public  RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
