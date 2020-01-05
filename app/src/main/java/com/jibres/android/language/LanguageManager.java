274cba8a325a69a0d677db5890d0caf43f79ebed
amingoli78 <amin.goli78@gmail.com>
2020-01-05 19:35:17 +0330

    add Language Manager

---
+++ app/src/main/java/com/jibres/android/language/LanguageManager.java
@@ -0,0 +1,46 @@
+package com.jibres.android.language;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LanguageManager extends ContextWrapper {
+
+  SharedPreferences.Editor editor;
+  SharedPreferences sharedPreferences;
+  public static final String SH_PREF_NAME = "ShPerfManager_Jibres_LanguageManager";
+
+
+  @SuppressLint("CommitPrefEdits")
+  private LanguageManager(Context context) {
+    super(context);
+    sharedPreferences = getSharedPreferences(SH_PREF_NAME, MODE_PRIVATE);
+    editor = sharedPreferences.edit();
+  }
+  public static LanguageManager get(Context context) {
+    return new LanguageManager(context);
+  }
+
+  /** App Info */
+  public static final String appLanguage = "appLanguage";
+
+  public void save_app_language(String languageLocal) {
+    editor.putString(appLanguage, languageLocal);
+    editor.apply();
+  }
+
+  public Map<String, String> getAppLanguage() {
+    HashMap<String, String> hashMap = new HashMap<>();
+    hashMap.put(appLanguage, sharedPreferences.getString(appLanguage, "en" ));
+    return hashMap;
+  }
+
+
+}
+
+
