fa2cee2dd9541f5917b2347c40add1e0447a9ea8
amingoli78 <amin.goli78@gmail.com>
2020-01-07 18:16:34 +0330

    add Chaker For Internet

--- app/src/main/java/com/jibres/android/function/Chake.java
+++ app/src/main/java/com/jibres/android/function/Chake.java
@@ -1,8 +1,16 @@
 package com.jibres.android.function;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import com.jibres.android.managers.UserManager;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Objects;
 
 public class Chake {
     public static Boolean userIsAddTemp(Context context){
@@ -11,10 +19,31 @@ public class Chake {
                 && UserManager.getZonId(context) !=null ;
     }
 
-    public static Boolean userIsAddMobile(Context context){
+    public static Boolean userHasMobileNumber(Context context){
         return UserManager.getApikey(context) !=null
                 && UserManager.getUserCode(context) !=null
                 && UserManager.getZonId(context) !=null
                 && UserManager.getMobile(context) !=null ;
+    }
+
+    public static boolean internet(Context context){
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo =
+                Objects.requireNonNull(connectivityManager).getActiveNetworkInfo();
+        if (activeNetworkInfo!=null && activeNetworkInfo.isConnected()){
+            try {
+                int timeoutMs = 1500;
+                Socket sock = new Socket();
+                SocketAddress sockaddr = new InetSocketAddress("8.8.8.8", 53);
+                sock.connect(sockaddr, timeoutMs);
+                sock.close();
+                return true;
+            } catch (IOException e) {
+                return false;
+            }
+        }else {
+            return false;
+        }
     }
 }
