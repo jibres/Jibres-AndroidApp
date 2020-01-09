804d9449e3a641cd279431d799f93ec88e73d62d
amingoli78 <amin.goli78@gmail.com>
2020-01-09 17:41:04 +0330

    add Setting Activity

---
+++ app/src/main/java/com/jibres/android/activity/securitysetting/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.jibres.android.activity.securitysetting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jibres.android.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
