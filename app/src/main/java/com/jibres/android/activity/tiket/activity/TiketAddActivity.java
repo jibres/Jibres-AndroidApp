6ef1947235b94bb673791ba939fad02ded973e6e
amingoli78 <amin.goli78@gmail.com>
2020-01-13 19:45:46 +0330

    fix xml AddTiket

--- app/src/main/java/com/jibres/android/activity/tiket/activity/TiketAddActivity.java
+++ app/src/main/java/com/jibres/android/activity/tiket/activity/TiketAddActivity.java
@@ -1,8 +1,11 @@
 package com.jibres.android.activity.tiket.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.jibres.android.R;
 
@@ -12,5 +15,21 @@ public class TiketAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiket_add);
+
+        View toolbarView = findViewById(R.id.toolbar);
+        Toolbar toolbar = toolbarView.findViewById(R.id.toolbar);
+        TextView mTitle = toolbar.findViewById(R.id.title);
+        setSupportActionBar(toolbar);
+        mTitle.setText("ارسال تیکت");
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
