71d3e91f09b44234b40502be6c4078e8c58cae13
amingoli78 <amin.goli78@gmail.com>
2020-01-26 11:21:16 +0330

    add Gradient For TextView

--- app/src/main/java/com/jibres/android/utility/ColorUtil.java
+++ app/src/main/java/com/jibres/android/utility/ColorUtil.java
@@ -1,8 +1,12 @@
 package com.jibres.android.utility;
 
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.graphics.drawable.GradientDrawable;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class ColorUtil {
 
@@ -15,5 +19,31 @@ public class ColorUtil {
                         Color.parseColor(END)
                 });
         view.setBackgroundDrawable(gradient);
+    }
+
+    public static void setGradient(View view, String START , String END, Boolean setLeftToRight){
+        if (setLeftToRight){
+            GradientDrawable gradient = new GradientDrawable(
+                    GradientDrawable.Orientation.LEFT_RIGHT,
+                    new int[] {
+                            Color.parseColor(START),
+                            Color.parseColor(END)
+                    });
+            view.setBackgroundDrawable(gradient);
+        }else {
+         setGradient(view,START,END);
+        }
+    }
+
+    public static void setGradient(TextView textView, String START , String END){
+        textView.measure(0, 0);       //must call measure!
+        int h = textView.getMeasuredHeight(); //get height
+        int w = textView.getMeasuredWidth()/2;  //get width
+        Log.e("amingoli", h+"setGradient: "+w );
+        textView.getPaint().setShader(
+                new LinearGradient(0, 0, w, h,
+                Color.parseColor(START),
+                Color.parseColor(END),
+                Shader.TileMode.CLAMP));
     }
 }
