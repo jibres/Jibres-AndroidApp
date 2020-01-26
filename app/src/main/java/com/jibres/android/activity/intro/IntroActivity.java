1148338ff967ed60d82471cffd81d28d4b40c091
amingoli78 <amin.goli78@gmail.com>
2020-01-26 18:51:02 +0330

    fix

--- app/src/main/java/com/jibres/android/activity/intro/IntroActivity.java
+++ app/src/main/java/com/jibres/android/activity/intro/IntroActivity.java
@@ -28,21 +28,14 @@ public class IntroActivity extends AppCompatActivity {
     RecyclerViewPager recyclerViewPager;
     IntroAdapter adaptorIntro;
 
-    Button nex, prav;
-    String nex_string, pravs_string, skip_string;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         AppManager.get(getApplication()).save_splash(3);
 
-        nex_string = "->";
-        pravs_string = "<-";
-        skip_string = "SKIP";
 
-        nex = findViewById(R.id.btn_next);
-        prav = findViewById(R.id.btn_prav);
+
 
         itemIntroList = new ArrayList<>();
         recyclerViewPager  = findViewById(R.id.recyclerViewPager_intro);
@@ -73,66 +66,6 @@ public class IntroActivity extends AppCompatActivity {
                         itemIntroList.add(new IntroModel(image,title,desc,bg_from,bg_to,title_color,desc_color));
                         recyclerViewPager.setLayoutManager(layout);
                         recyclerViewPager.setItemAnimator(new DefaultItemAnimator());
-
-                        if (!object.isNull("btn")){
-                            JSONArray btnArray = object.getJSONArray("btn");
-                            for (int j = 0; j < btnArray.length(); j++) {
-                                JSONObject btnObject = btnArray.getJSONObject(j);
-                                String action = btnObject.getString("action");
-                                String titleBtn = btnObject.getString("title");
-
-                                switch (action){
-                                    case "next":
-                                        nex_string = titleBtn;
-                                        break;
-                                    case "prev":
-                                        pravs_string = titleBtn;
-                                        break;
-                                    case "start":
-                                        skip_string = titleBtn;
-                                        break;
-                                }
-                            }
-                        }
-                    }
-
-                    if (itemIntroList.size() == 1){
-                        nex.setText(nex_string);
-                        nex.setOnClickListener(view -> {
-                            finish();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        });
-                    }
-                    else {
-                        nex.setText(nex_string);
-                        prav.setText(pravs_string);
-                        nex.setOnClickListener(view -> {
-
-                            if (page_intro() == itemIntroList.size()-1){
-                                finish();
-                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                            }else {
-                                recyclerViewPager.smoothScrollToPosition(recyclerViewPager.getCurrentPosition() + 1);
-                            }
-                        });
-                        prav.setOnClickListener(view ->
-                                recyclerViewPager.smoothScrollToPosition(recyclerViewPager.getCurrentPosition() - 1));
-
-                        recyclerViewPager.addOnPageChangedListener((i, i1) -> {
-                            if (recyclerViewPager.isScrollContainer()){
-                                if (page_intro() == itemIntroList.size()-1){
-                                    nex.setText(skip_string);
-                                }else {
-                                    nex.setText(nex_string);
-                                }
-
-                                if (page_intro() >=1){
-                                    prav.setVisibility(View.VISIBLE);
-                                }else {
-                                    prav.setVisibility(View.INVISIBLE);
-                                }
-                            }
-                        });
                     }
 
                 } catch (JSONException e) {
