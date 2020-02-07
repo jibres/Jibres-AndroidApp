e03587d8d8f1d183022cb75ce1ed3ac8bc41dd4a
amingoli78 <amin.goli78@gmail.com>
2020-02-07 17:11:29 +0330

    load only url started by "http"and "https"

--- app/src/main/java/com/jibres/android/activity/WebViewActivity.java
+++ app/src/main/java/com/jibres/android/activity/WebViewActivity.java
@@ -2,15 +2,121 @@ package com.jibres.android.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.jibres.android.R;
+import com.jibres.android.activity.intro.IntroActivity;
+import com.jibres.android.managers.UrlManager;
 
-public class WebViewActivity extends AppCompatActivity {
+import im.delight.android.webview.AdvancedWebView;
+
+public class WebViewActivity extends AppCompatActivity implements AdvancedWebView.Listener {
+
+    String TAG = "amingoli";
+    private AdvancedWebView mWebView;
+    String url = "https://jibres.com/dashboard";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        url = getIntent().getStringExtra("url");
+
+        mWebView = findViewById(R.id.webview);
+        mWebView.setListener(this, this);
+        mWebView.loadUrl(url);
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.d(TAG, "shouldOverrideUrlLoading: "+url);
+                if (!url.startsWith("https://")||!url.startsWith("http://")){
+                    Intent intent = null;
+                    switch (url){
+                        case "jibres://intro":
+                            intent = new Intent(WebViewActivity.this,IntroActivity.class);
+                            break;
+                        case "jibres://test":
+                            intent = new Intent(WebViewActivity.this,MainActivity.class);
+                            break;
+                    }
+                    if (intent!=null){
+                        startActivity(intent);
+                    }
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+        // ...
     }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+        // ...
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onPause() {
+        mWebView.onPause();
+        // ...
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mWebView.onDestroy();
+        // ...
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        mWebView.onActivityResult(requestCode, resultCode, intent);
+        Log.d(TAG, "onActivityResult: "+requestCode+"\n"+resultCode+"\n"+intent);
+        // ...
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!mWebView.onBackPressed()) { return; }
+        // ...
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+        Log.d(TAG, "onPageStarted: "+url);
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+        Log.d(TAG, "onPageFinished: "+url);
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+        Log.d(TAG, "onPageError: "+errorCode+"\n"+description+"\n"+failingUrl);
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) { }
+
+    @Override
+    public void onExternalPageRequest(String url) { }
+
 }
