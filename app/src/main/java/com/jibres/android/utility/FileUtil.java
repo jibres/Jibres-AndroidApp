afd074cf6e137c4d0c25fa6423d43f8ad5568523
amingoli78 <amin.goli78@gmail.com>
2020-02-20 12:23:37 +0330

    update ReadFile

---
+++ app/src/main/java/com/jibres/android/utility/FileUtil.java
@@ -0,0 +1,34 @@
+package com.jibres.android.utility;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileUtil {
+    public static String ReadFileAssets(Context context, String FileName){
+        try {
+            InputStream is = context.getAssets().open(FileName);
+            // We guarantee that the available method returns the total
+            // size of the asset...  of course, this does mean that a single
+            // asset can't be more than 2 gigs.
+            int size = is.available();
+            // Read the entire asset into a local byte buffer.
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            // Convert the buffer into a string.
+            String text = new String(buffer);
+            // Finally stick the string into the text view.
+            // Replace with whatever you need to have the text into.
+            if (text.length()==0){
+                return null;
+            }else {
+                return text;
+            }
+        } catch (IOException e) {
+            // Should never happen!
+            throw new RuntimeException(e);
+        }
+    }
+}
--- app/src/main/java/com/jibres/android/utility/SecretReadFile.java
+++ app/src/main/java/com/jibres/android/utility/SecretReadFile.java
@@ -1,14 +1,9 @@
 package com.jibres.android.utility;
 
 import android.content.Context;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class SecretReadFile {
+    private static String folder_secret = "secret";
     private static String endpoint = "/endpoint.txt";
     private static String endpoint_test = "/endpoint_test.txt";
     private static String store = "/store.txt";
@@ -24,28 +19,6 @@ public class SecretReadFile {
     }
 
     private static String ReadFile(Context context,String FileName){
-        try {
-            InputStream is = context.getAssets().open("secret"+FileName);
-            // We guarantee that the available method returns the total
-            // size of the asset...  of course, this does mean that a single
-            // asset can't be more than 2 gigs.
-            int size = is.available();
-            // Read the entire asset into a local byte buffer.
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            // Convert the buffer into a string.
-            String text = new String(buffer);
-            // Finally stick the string into the text view.
-            // Replace with whatever you need to have the text into.
-            if (text.length()==0){
-                return null;
-            }else {
-                return text;
-            }
-        } catch (IOException e) {
-            // Should never happen!
-            throw new RuntimeException(e);
-        }
+        return FileUtil.ReadFileAssets(context,folder_secret+FileName);
     }
 }
